dd00af9648ea3d5a77df00e35390c738504aa8e2
Sally Resch <sallyresch27@gmail.com>
2022-04-12 21:19:44 +0200

    made list appear when typing chars in searchbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 // getting all required elements
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+
+//if user press any key and release
+inputBox.onkeyup = (e) => {
+  let userData = e.target.value; //user entered data
+  let emptyArray = [];
+  if (userData) {
+    emptyArray = suggestions.filter((data) => {
+      //filtering array value and user char to lowercase and returns only 
+      //word/sentc which starts with user entered word
+      return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+    });
+    emptyArray = emptyArray.map((data) => {
+      return data = '<li>' + data + '</li>';
+    });
+    console.log(emptyArray);
+    searchWrapper.classList.add("active");
+  } else {
+
+  }
+  showSuggestions(emptyArray);
+}
+
+function showSuggestions(list) {
+  let listData;
+  if (!list.length) {
+    userValue = inputBox.value;
+    listData = '<li>' + userValue + '</li>';
+  } else {
+    listData = list.join('');
+  }
+  suggBox.innerHTML = listData;
+}
